780c73ed009a9831b3df505c7846dac5c353918d
max <max>
2025-12-24 17:28:18 +0700

    Refactor HomePage and Hero components by removing unused elements and updating layout structure; enhance utility CSS for improved responsiveness.

--- src/modules/HomePage/Hero/index.tsx
+++ src/modules/HomePage/Hero/index.tsx
@@ -1,20 +1,18 @@
 'use client';
 
 import { Container, GridContainer } from '@/components/Container';
-import Text from '@/components/ui/text';
 import type React from 'react';
 
 function Hero(): React.ReactElement {
   return (
     <Container className="mt-[149px] laptop:mt-[126px] desktop:mt-[170px]">
-      <Container>
-        <Text as="h1" size={30} color={'grey-60'} weight={'medium'} font={'heading'}>
-          Hello
-        </Text>
-      </Container>
-      <GridContainer>
-        <div>left content</div>
-        <div>right content</div>
+      <GridContainer className="">
+        <div className="col-span-7 pt-[40px] laptop:pt-[100px] desktop:pt-[150px]">
+          left content
+        </div>
+        <div className="col-span-5 border-l border-dark-15 pt-[150px] laptop:pl-[80px]">
+          right content
+        </div>
       </GridContainer>
     </Container>
   );
